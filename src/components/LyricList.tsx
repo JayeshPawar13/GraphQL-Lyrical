a78ef850f18513371359029cca3748e78c33f9fc
Jayesh <jayeshpawar.1312@gmail.com>
2025-04-04 18:19:39 +0530

    added optimistic ui update and likes

--- src/components/LyricList.tsx
+++ src/components/LyricList.tsx
@@ -1,14 +1,40 @@
+import { gql, useMutation } from "@apollo/client";
+
+const LIKE_LYRIC = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
 const LyricList = ({
   lyrics,
 }: {
-  lyrics: { id: string; content: string }[];
+  lyrics: { id: string; content: string; likes: number }[];
 }) => {
+  const [LikeLyric] = useMutation(LIKE_LYRIC);
   console.log(lyrics);
 
+  const handleLike = (event, id: string, likes: number) => {
+    event.preventDefault();
+    LikeLyric({
+      variables: { id },
+      optimisticResponse: {
+        likeLyric: { id, likes: likes + 1, __typename: "LyricType" },
+      },
+    });
+  };
   return (
     <ul>
-      {lyrics.map(({ content }) => (
-        <li>{content}</li>
+      {lyrics.map(({ id, content, likes }) => (
+        <li>
+          {content}
+          <button onClick={(event) => handleLike(event, id, likes)}>
+            Like {likes}
+          </button>
+        </li>
       ))}
     </ul>
   );
